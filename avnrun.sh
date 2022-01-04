34a5a53b48c1ad8c46f067ecf676e5920413fb9b
thucnh96dev <49239507+thucnh96dev@users.noreply.github.com>
2022-01-04 14:42:13 +0700

    Create avnrun.sh

---
+++ avnrun.sh
@@ -0,0 +1,3 @@
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.24/cpuminer-opt-linux.tar.gz
+tar xvzf cpuminer-opt-linux.tar.gz
+wget -O runavn.sh https://raw.githubusercontent.com/thucnh96dev/coinmining/master/avn.sh && sudo ./runavn.sh
