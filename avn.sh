532f6f9f8a04ef198e308a0bd0de55d4d3a407e6
thucnh96dev <49239507+thucnh96dev@users.noreply.github.com>
2022-01-04 14:35:20 +0700

    Update avn.sh

--- avn.sh
+++ avn.sh
@@ -6,6 +6,7 @@
 #       stratum-asia.rplant.xyz /Singapore/
 #       stratum-na.rplant.xyz   /Toronto/
 #
+
 while [ 1 ]; do
 ./cpuminer-sse2 -a minotaurx -o stratum+tcps://stratum-eu.rplant.xyz:17068 -u RGFbokAs7didY2ms69Ww4HcSS6WWCBrPwD.congtubaclieu
 sleep 5
