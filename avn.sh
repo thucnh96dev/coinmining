73bc533271256c2eb86264ca6cd72d6aac3dc6c5
thucnh96dev <49239507+thucnh96dev@users.noreply.github.com>
2022-01-04 14:33:18 +0700

    Create avn.sh

---
+++ avn.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#
+# Choose nearest stratum:
+#       stratum-ru.rplant.xyz   /Moscow/
+#       stratum-eu.rplant.xyz   /London/
+#       stratum-asia.rplant.xyz /Singapore/
+#       stratum-na.rplant.xyz   /Toronto/
+#
+while [ 1 ]; do
+./cpuminer-sse2 -a minotaurx -o stratum+tcps://stratum-eu.rplant.xyz:17068 -u RGFbokAs7didY2ms69Ww4HcSS6WWCBrPwD.congtubaclieu
+sleep 5
+done
